965730172c680255d736218027fe3a609ced2bd0
Ancheta-VashPatrick <vbancheta@up.edu.ph>
2022-10-24 09:11:29 +0000

    AutoAugment only in training

--- Reproducibility/litdataloader.py
+++ Reproducibility/litdataloader.py
@@ -16,25 +16,39 @@ class ImageNetDataModule(LightningDataModule):
         self.transform = transform
 
     def prepare_data(self):
+        training_transforms, testing_transforms = None, None
         if self.transform is None:
-            self.transform = transforms.Compose([
+            image_transforms = transforms.Compose([
                 transforms.Resize(256),
                 transforms.CenterCrop(224),
-                transforms.AutoAugment(),
+            ])
+            tensor_transforms = transforms.Compose([
                 transforms.ToTensor(),
                 transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                      std=[0.229, 0.224, 0.225]),
             ])
+            training_transforms = transforms.Compose([
+                image_transforms,
+                transforms.AutoAugment(),
+                tensor_transforms,
+            ])
+            testing_transforms = transforms.Compose([
+                image_transforms,
+                tensor_transforms,
+            ])
+        else:
+            training_transforms = self.transform
+            testing_transforms = self.transform
 
         self.train_dataset = datasets.ImageNet(
-            "/data/imagenet/", split='train', transform=self.transform)
+            "/data/imagenet/", split='train', transform=training_transforms)
 
         # validation step is the same as test step
         self.val_dataset = datasets.ImageNet(
-            "/data/imagenet/", split='val', transform=self.transform)
+            "/data/imagenet/", split='val', transform=testing_transforms)
 
         self.test_dataset = datasets.ImageNet(
-            "/data/imagenet/", split='val', transform=self.transform)
+            "/data/imagenet/", split='val', transform=testing_transforms)
 
     def setup(self, stage=None):
         self.prepare_data()
@@ -45,7 +59,7 @@ class ImageNetDataModule(LightningDataModule):
             batch_size=self.batch_size,
             num_workers=self.num_workers,
             shuffle=True,
-            pin_memory=True,
+            pin_memory=True
         )
 
     def val_dataloader(self):
